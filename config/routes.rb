51b143175a22deb20649791273672d967aa1a576
hdwatts <howard@hdwatts.com>
2016-12-26 15:21:03 -0500

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'application#index'
   get '/index', to: 'application#index'
   post '/upload', to: 'upload#index'
   post '/getcolor', to: 'upload#color'
